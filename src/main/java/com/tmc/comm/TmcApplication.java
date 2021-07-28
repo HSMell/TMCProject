3e5738aeec1cddbd8892bbe8b0686ab8ca6de56e
GnVio <hyde2598@gmail.com>
2021-07-28 19:01:52 +0900

    TMC PROJECT

---
+++ src/main/java/com/tmc/comm/TmcApplication.java
@@ -0,0 +1,13 @@
+package com.tmc.comm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tmc/comm/TmcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmc.comm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
